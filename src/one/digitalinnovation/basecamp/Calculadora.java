fe0b391fbda20b27515614e78cafe0f131b6ff73
NiltonFerraz <nilferraz@gmail.com>
2022-04-19 10:10:19 -0300

    Aula Métodos

---
+++ src/one/digitalinnovation/basecamp/Calculadora.java
@@ -0,0 +1,30 @@
+package one.digitalinnovation.basecamp;
+
+public class Calculadora {
+
+	public static void soma(double num1, double num2) {
+		
+		double resul = num1 + num2;
+		System.out.println("Soma: " + resul);
+	}
+	
+	public static void multiplicacao(double num1, double num2) {
+		
+		double resul = num1 * num2;
+		System.out.println("Multiplicação: " + resul);
+	}
+	
+	public static void divisao(double num1, double num2) {
+		
+		double resul = num1 / num2;
+		System.out.println("Divisão: " + resul);
+	}
+
+	public static void subtracao(double num1, double num2) {
+		
+		double resul = num1 - num2;
+		System.out.println("Subtração: " + resul);
+	}
+	
+	
+}
